6eea6b7190f5d856ab99d047ed6afffc916e1dc9
NikitaED <Nik.lesinc@yandex.ru>
2024-09-01 20:17:49 +0500

    Add files via upload

---
+++ module_1.6.py
@@ -0,0 +1,15 @@
+my_dict={'Nikita':2005, 'Igor':2002, 'Sofia':2003}
+print(my_dict)
+print(my_dict['Igor'])
+my_dict['Den']=1999
+print(my_dict)
+my_dict['Sasha'] = 2004
+my_dict['Ivan'] = 2001
+del my_dict['Ivan']
+print(my_dict['Sasha'])
+print(my_dict)
+
+my_set= {'Peach', 'Peach', 1, 1, 2, 2, True, True}
+print(my_set.add(5))
+print(my_set.discard(1))
+print(my_set)
